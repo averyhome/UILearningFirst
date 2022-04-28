812c2b5608bd3203017c848f6779cd65ff823983
mac <330578304@qq.com>
2022-04-28 11:43:04 +0800

    init

---
+++ UILearningFirst/UILearningFirst/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  UILearningFirst
+//hhh
+//  Created by mac on 2022/4/28.
+//  Copyright © 2022 school. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ UILearningFirst/UILearningFirst/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UILearningFirst
+//
+//  Created by mac on 2022/4/28.
+//  Copyright © 2022 school. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
